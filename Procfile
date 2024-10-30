fe9b93e58cbf0bd5456b57e5a951861e42721738
Michael Bianco <iloveitaly@gmail.com>
2024-10-30 15:22:27 -0600

    celery config working

---
+++ Procfile
@@ -0,0 +1,3 @@
+api: fastapi run
+worker: celery -A app.jobs worker
+scheduler: celery -A app.jobs beat
--- app/configuration/database.py
+++ app/configuration/database.py
@@ -2,7 +2,7 @@ from decouple import config
 from sqlalchemy import create_engine
 from sqlmodel import Session, SQLModel, text
 
-from .environments import is_production, is_testing
+from ..environments import is_production, is_testing
 
 
 def database_url():
