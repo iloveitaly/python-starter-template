769c82d41b216fc29c1aeb7d934c08c404092c48
Michael Bianco <iloveitaly@gmail.com>
2024-11-05 14:23:00 -0700

    remove server config since we are client only

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,18 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"
+import Terminal from "vite-plugin-terminal"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+import { reactRouter } from "@react-router/dev/vite"
+import { sentryVitePlugin } from "@sentry/vite-plugin"
 
 export default defineConfig({
-  plugins: [reactRouter({ssr: false}), tsconfigPaths()],
-});
+  plugins: [
+    sentryVitePlugin({
+      // real component names in errors
+      reactComponentAnnotation: { enabled: true },
+    }),
+    Terminal(),
+    reactRouter({ ssr: false }),
+    tsconfigPaths(),
+  ],
+})
