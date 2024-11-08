7030b9634cb72286bc8b6f5aec01d6e59cc4cf8a
Michael Bianco <iloveitaly@gmail.com>
2024-11-08 16:48:21 -0700

    feat(logging): enhance logger docstring and format cleanup

--- app/configuration/logging.py
+++ app/configuration/logging.py
@@ -22,23 +22,32 @@ def _logger_factory():
 
     # allow user to specify a log in case they want to do something meaningful with the stdout
     if python_log_path := config("PYTHON_LOG_PATH", default=None):
-        python_log = open(
-            python_log_path, "a", encoding="utf-8"
-        )  # pylint: disable=consider-using-with
+        python_log = open(python_log_path, "a", encoding="utf-8")
         logger_factory = structlog.PrintLoggerFactory(file=python_log)
 
     return logger_factory
 
 
 def configure_logger():
+    """
+    Create a struct logger with some special additions:
+
+    with log.context(key=value):
+        log.info("some message")
+
+    log.local(key=value)
+    log.info("some message")
+    log.clear()
+    """
+
     log = structlog.get_logger()
 
     # context manager to auto-clear context
-    log.context = structlog.contextvars.bound_contextvars  # type: ignore
+    log.context = structlog.contextvars.bound_contextvars
     # set thread-local context
-    log.local = structlog.contextvars.bind_contextvars  # type: ignore
+    log.local = structlog.contextvars.bind_contextvars
     # clear thread-local context
-    log.clear = structlog.contextvars.clear_contextvars  # type: ignore
+    log.clear = structlog.contextvars.clear_contextvars
 
     log_level = config("LOG_LEVEL", default="INFO", cast=str)
     level = getattr(logging, log_level.upper())
