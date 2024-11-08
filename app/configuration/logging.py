52e519081033aad4eddab29253d8595d98cfca56
Michael Bianco <iloveitaly@gmail.com>
2024-11-08 16:23:47 -0700

    feat(logging): update logger for env-specific strategies
    - Updated `_logger_factory` for better environment handling
    - Optimized logging in production with `BytesLoggerFactory`
    - Development logs to stdout or file via `PYTHON_LOG_PATH`
    - Configured JSON renderer for production logs
    - Added console renderer for development logs
    - Included `orjson` dependency version `3.10.11`

--- app/configuration/logging.py
+++ app/configuration/logging.py
@@ -1,8 +1,33 @@
 import logging
-import typing as t
 
+import orjson
 import structlog
+import structlog.dev
 from decouple import config
+
+from ..environments import is_production
+
+
+def _logger_factory():
+    """
+    Allow dev users to redirect logs to a file using PYTHON_LOG_PATH
+
+    In production, optimized for speed (https://www.structlog.org/en/stable/performance.html)
+    """
+
+    if is_production():
+        return structlog.BytesLoggerFactory()
+
+    logger_factory = structlog.PrintLoggerFactory()
+
+    # allow user to specify a log in case they want to do something meaningful with the stdout
+    if python_log_path := config("PYTHON_LOG_PATH", default=None):
+        python_log = open(
+            python_log_path, "a", encoding="utf-8"
+        )  # pylint: disable=consider-using-with
+        logger_factory = structlog.PrintLoggerFactory(file=python_log)
+
+    return logger_factory
 
 
 def configure_logger():
@@ -15,30 +40,27 @@ def configure_logger():
     # clear thread-local context
     log.clear = structlog.contextvars.clear_contextvars  # type: ignore
 
-    logger_factory = structlog.PrintLoggerFactory()
-
-    # allow user to specify a log in case they want to do something meaningful with the stdout
-    if python_log_path := config("PYTHON_LOG_PATH", default=None):
-        python_log = open(
-            python_log_path, "a", encoding="utf-8"
-        )  # pylint: disable=consider-using-with
-        logger_factory = structlog.PrintLoggerFactory(file=python_log)
-
-    log_level = t.cast(str, config("LOG_LEVEL", default="INFO", cast=str))
+    log_level = config("LOG_LEVEL", default="INFO", cast=str)
     level = getattr(logging, log_level.upper())
 
-    # TODO logging.root.manager.loggerDict
     # we need this option to be set for other non-structlog loggers
     logging.basicConfig(level=level)
 
-    # TODO look into further customized format
-    # https://cs.github.com/GeoscienceAustralia/digitalearthau/blob/4cf486eb2a93d7de23f86ce6de0c3af549fe42a9/digitalearthau/uiutil.py#L45
-
     structlog.configure(
-        context_class=dict,
-        wrapper_class=structlog.make_filtering_bound_logger(level),
-        logger_factory=logger_factory,
         cache_logger_on_first_use=True,
+        wrapper_class=structlog.make_filtering_bound_logger(level),
+        logger_factory=_logger_factory(),
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.format_exc_info,
+            structlog.processors.TimeStamper(fmt="iso", utc=True),
+            (
+                structlog.dev.ConsoleRenderer()
+                if not is_production()
+                else structlog.processors.JSONRenderer(serializer=orjson.dumps)
+            ),
+        ],
     )
 
     return log
