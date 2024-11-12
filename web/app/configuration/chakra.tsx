60b0489da5694f64d94d7013315412e4b6b69986
Michael Bianco <iloveitaly@gmail.com>
2024-11-12 12:22:35 -0700

    refactor: remove Chakra UI integration and dependencies

--- web/app/configuration/chakra.tsx
+++
@@ -1,9 +0,0 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-
-export default function withChakraProvider(Component: React.ComponentType) {
-  return (props: any) => (
-    <ChakraProvider value={defaultSystem}>
-      <Component {...props} />
-    </ChakraProvider>
-  )
-}
