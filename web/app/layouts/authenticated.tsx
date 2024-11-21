01f2489fc5679c369e15530778958a01184176a3
Michael Bianco <iloveitaly@gmail.com>
2024-11-21 16:06:22 -0700

    feat: add authenticated layout and utility functions
    Generated-by: aiautocommit

---
+++ web/app/layouts/authenticated.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router"
+
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react"
+
+export default function AuthenticatedLayout() {
+  // https://clerk.com/docs/components/control/redirect-to-signin
+  return (
+    <>
+      <SignedIn>
+        <Outlet />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
+  )
+}
---
+++ web/app/lib/utils.ts
@@ -0,0 +1,18 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+// from: https://ui.shadcn.com/docs/installation/manual
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+// type Id<T> = T extends object ? {} & { [P in keyof T]: Id<T[P]> } : T
+// expands object types one level deep
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type ExpandRecursively<T> = T extends object
+  ? T extends infer O
+    ? { [K in keyof O]: ExpandRecursively<O[K]> }
+    : never
+  : T
