1eb4f105e7071c7de8ccaf11786859c5c6845f21
Michael Bianco <iloveitaly@gmail.com>
2024-11-26 07:01:57 -0700

    refactor: move db cleaning to activemodel
    Generated-by: aiautocommit

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,17 @@
 
 import os
 
+# when running locally, switching to the full-blown CI environment is a pain
+# to make it quick & easy to run tests, we force the environment to test
+# this will not be the same as CI, but it's closer and faster for devprod
 if os.environ["PYTHON_ENV"] != "test":
-    print("PYTHON_ENV is not set to test, forcing")
+    print("\033[91mPYTHON_ENV is not set to test, forcing\033[0m")
     os.environ["PYTHON_ENV"] = "test"
 
 import typing as t
 
 import pytest
-from activemodel import SessionManager
+from activemodel.pytest import database_reset_transaction, database_reset_truncate
 from decouple import config
 from fastapi.testclient import TestClient
 from structlog import get_logger
@@ -17,6 +20,7 @@ from structlog import get_logger
 # important to ensure model metadata is added to the application
 import app.models  # noqa: F401
 
+# TODO set logger name, not context
 log = get_logger(test=True)
 
 
@@ -30,6 +34,10 @@ log = get_logger(test=True)
 # @pytest.fixture(scope="session")
 # def event_loop_policy():
 #     return uvloop.EventLoopPolicy()
+
+
+def pytest_configure(config):
+    database_reset_truncate()
 
 
 def base_server_url(protocol: t.Literal["http", "https"] = "http"):
@@ -58,77 +66,4 @@ def client():
     return TestClient(api_app, base_url=base_server_url())
 
 
-from activemodel.pytest import truncate_db
-
-print("huh?")
-
-
-def pytest_configure(config):
-    truncate_db()
-
-
-# CLEANER_STRATEGY: t.Literal["truncate", "session"] = "truncate"
-
-
-# if CLEANER_STRATEGY == "truncate":
-
-#     @pytest.fixture(scope="session", autouse=True)
-#     def truncate_db_tests():
-#         """
-#         Problem with truncation is you can't run multiple tests in parallel
-#         """
-
-#         truncate_db()
-
-#         yield
-
-
-# if CLEANER_STRATEGY == "session":
-
-
-@pytest.fixture(scope="function", autouse=True)
-def reset_db():
-    """
-    https://stackoverflow.com/questions/62433018/how-to-make-sqlalchemy-transaction-rollback-drop-tables-it-created
-    https://aalvarez.me/posts/setting-up-a-sqlalchemy-and-pytest-based-test-suite/
-    https://github.com/nickjj/docker-flask-example/blob/93af9f4fbf185098ffb1d120ee0693abcd77a38b/test/conftest.py#L77
-    https://github.com/caiola/vinhos.com/blob/c47d0a5d7a4bf290c1b726561d1e8f5d2ac29bc8/backend/test/conftest.py#L46
-    """
-
-    engine = SessionManager.get_instance().get_engine()
-
-    with engine.begin() as connection:
-        transaction = connection.begin_nested()
-
-        SessionManager.get_instance().session_connection = connection
-
-        try:
-            yield
-        finally:
-            transaction.rollback()
-            connection.close()
-
-
-#     print("reset_db complete")
-
-
-# TODO the problem with this approach is not every connection uses the same session
-# @pytest.fixture(scope="session", autouse=True)
-# def reset_db():
-#     start_truncation_query = """
-#         BEGIN;
-#         SAVEPOINT test_truncation_savepoint;
-#     """
-
-#     raw_sql_exec(start_truncation_query)
-
-#     yield
-
-#     end_truncation_query = """
-#         ROLLBACK TO SAVEPOINT test_truncation_savepoint;
-#         RELEASE SAVEPOINT test_truncation_savepoint;
-#         ROLLBACK;
-#     """
-
-#     raw_sql_exec(end_truncation_query)
-#     print("reset_db complete!")
+pytest.fixture(scope="function", autouse=True)(database_reset_transaction)
