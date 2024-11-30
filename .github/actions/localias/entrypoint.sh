40872829f60c4f591f66753378c262fad9ae063f
Michael Bianco <iloveitaly@gmail.com>
2024-11-30 12:22:29 -0700

    fix: streamline localias entrypoint script
    - Removed commented-out debug checks for NSS DB files.
    - Simplified certificate installation process by removing sleep.
    - Adjusted comments for clarity on certificate paths and sudo usage.
    
    Generated-by: aiautocommit

--- .github/actions/localias/entrypoint.sh
+++ .github/actions/localias/entrypoint.sh
@@ -6,16 +6,8 @@ banner_echo() {
   printf "\n\033[0;36m%s   \033[0m\n" "$1"
 }
 
-# when this directory is properly configured, you should see the following files: cert9.db  key4.db  pkcs11.txt
-# [ ! -d "$HOME/.pki/nssdb" ] && mkdir -p "$HOME/.pki/nssdb" && certutil -d sql:$HOME/.pki/nssdb -N --empty-password
-
-# banner_echo "Checking for NSS DB..."
-# ls -l $HOME/.pki/nssdb
-
-# banner_echo "Installed certificates:"
-# certutil -L -d sql:${HOME}/.pki/nssdb
-
 if ! command -v localias >/dev/null & then
+  # TODO use hosted version https://github.com/peterldowns/localias/pull/44
   banner_echo "Installing localias"
   cat "$GITHUB_ACTION_PATH/install.sh" | sh -s -- --yes
 fi
@@ -23,17 +15,20 @@ fi
 banner_echo "where is the cert"
 sudo localias debug cert
 
+# to view logs, run detached `sudo localaias run &`
 banner_echo "Starting localias..."
-# don't run as daemon so we can see the logs
-sudo -E localias start
+sudo localias start
 
-# when the daemon has finished initializing
+# wait until the daemon has finished initializing
+# file is normally located at: /root/.local/state/localias/caddy/pki/authorities/local/root.crt
+
 cert_location=$(sudo localias debug cert)
 daemon_success=false
 
 for i in {1..5}; do
   banner_echo "Checking for self-signed cert: $cert_location..."
 
+  # NOTE sudo is really important here, without this the check will fail since the file is protected
   if sudo [ -f "$cert_location" ]; then
     daemon_success=true
   else
@@ -52,13 +47,9 @@ $daemon_success || exit 1
 banner_echo "Refreshing system CA certs..."
 sudo update-ca-certificates --fresh
 
-# leave enough time for localias to initialize and generate certificates
-# TODO there's got to be a more deterministic way to handle this
-# sleep 15
-
 # certs are *not* installed by Caddy in the right location by default
 # specifically, we know this fixes `curl` so it respects our self-signed SSL certificates
-banner_echo "Installing locally signed cert"
+# banner_echo "Installing locally signed cert"
 # sudo localias debug cert --print | sudo tee -a /etc/ssl/certs/ca-certificates.crt
 
 # sudo update-ca-certificates --fresh
@@ -66,10 +57,11 @@ banner_echo "Installing locally signed cert"
 # https://stackoverflow.com/a/75352343/129415
 # TODO should we set this here? makes httpie work?
 # REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
-export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
-export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+# export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+# export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 # export NODE_EXTRA_CA_CERTS="/path/to/cert.pem"
 
+# TODO leave this here in case we are able to identify a link between time drifts and cert issues
 banner_echo "Datetime config..."
 timedatectl
 
