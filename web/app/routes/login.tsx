44cac089d944aaf65f1342dd4ba7165bdf53b02f
Michael Bianco <iloveitaly@gmail.com>
2024-12-23 13:27:16 -0700

    fix: update import for Clerk from react-router module
    Generated-by: aiautocommit

--- web/app/routes/login.tsx
+++ web/app/routes/login.tsx
@@ -6,16 +6,15 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
-} from "@clerk/clerk-react"
+} from "@clerk/react-router"
 
 export default function Index() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
       <SignedIn>
-        {/* <Navigate to="/home" /> https://discord.com/channels/856971667393609759/1311386772520960182 */}
-        <p>
-          <Link to="/home">Go Home</Link>
-        </p>
+        {/* TODO this is broken: https://discord.com/channels/856971667393609759/1311386772520960182 */}
+        {/* <Navigate to="/home" /> */}
+        <Link to="/home">Go Home</Link>
         <p>You are signed in!</p>
         <div>
           <p>View your profile here</p>
