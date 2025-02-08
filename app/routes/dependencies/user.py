cde3cbb7b31c1bf6d75d99954d9245aed172affd
Michael Bianco <iloveitaly@gmail.com>
2025-02-08 08:52:42 -0700

    fix: check user deletion with deleted_at attribute
    Generated-by: aiautocommit

--- app/routes/dependencies/user.py
+++ app/routes/dependencies/user.py
@@ -12,7 +12,7 @@ def inject_user_record(request: Request):
     clerk_id = request.state.auth_state.payload["sub"]
     user = User.find_or_create_by(clerk_id=clerk_id)
 
-    if user.deleted:
+    if user.deleted_at:
         raise HTTPException(
             status_code=status.HTTP_410_GONE, detail="Your Account has Been Disabled"
         )
