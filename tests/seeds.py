6e572fecbd5883b4ea4bd888218a1663ee39897f
Michael Bianco <iloveitaly@gmail.com>
2025-02-13 07:40:51 -0700

    feat(migrations): add user seeding to migrations and tests
    - Integrated `User` model seeding into the development, test migrations.
    - Logs and creates a `User` entity using `get_clerk_seed_user` for consistent test data setup.
    
    Generated-by: aiautocommit

--- tests/seeds.py
+++ tests/seeds.py
@@ -1,2 +1,12 @@
+from app.models.user import User
+
+from tests.routes.utils import get_clerk_seed_user
+from tests.utils import log
+
+
 def seed_test_data():
-    pass
+    ""
+    log.info("creating test fixture data")
+
+    username, password, user = get_clerk_seed_user()
+    user = User.find_or_create_by(clerk_id=user.id)  # noqa: F821
