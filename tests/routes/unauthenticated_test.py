e4361692d514ca3111f14af154429d41a4af1c66
Michael Bianco <iloveitaly@gmail.com>
2025-02-13 05:44:26 -0700

    test: add healthcheck and user status tests
    Generated-by: aiautocommit

--- tests/routes/unauthenticated_test.py
+++ tests/routes/unauthenticated_test.py
@@ -4,7 +4,14 @@ from app.server import api_app
 
 
 # NOTE why async? mostly to test async client tests
-async def test_https_redirection(aclient):
+async def test_healthcheck(aclient):
     response = await aclient.get(api_app.url_path_for("healthcheck"))
 
     assert response.status_code == status.HTTP_200_OK
+
+
+async def test_user_status(aclient):
+    response = await aclient.get(api_app.url_path_for("active_user_status"))
+
+    # should error since there are no users
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
