b5b9bbbfa9184be867f82a7dfd641f9498de0584
Michael Bianco <iloveitaly@gmail.com>
2025-02-21 17:34:15 -0700

    test: enhance javascript build logic for integration tests
    - Implemented a file modification time check to determine if a new JavaScript build is needed.
    - Introduced functionality to skip building if the JavaScript assets are up to date.
    - Enhanced logging for better insight into which files were inspected during the check.
    
    Generated-by: aiautocommit

--- tests/integration/javascript_build.py
+++ tests/integration/javascript_build.py
@@ -4,19 +4,62 @@ Integration tests are designed to run in an environment that closely mirrors pro
 Part of this means it uses production-build javascript assets. Ideally, these are always built
 *before* running the integration tests, but it's very easy to forget to do this when iterating on tests.
 
+We are doing some extra-fancy stuff here by calculating the more recent mtime of the files in web/
+and using that to determine if a new build should be kicked off. We will regret this at some point.
+
 In CI, the javascript assets are built before running any tests. Locally, we assume that you forgot
 and automatically kick off a build for you.
 """
 
+import os
 import subprocess
 import threading
 import time
 
+from gitignore_parser import parse_gitignore
+
+from app import root
 from app.environments import is_local_testing
 
 from tests.utils import log
 
 js_build_success = None
+
+# Build state file, the mtime is used to trigger new js builds
+BUILD_STATE_FILE = root / "tmp" / ".js_build_success"
+
+
+def get_latest_mtime(directory):
+    matches = parse_gitignore(directory / ".gitignore")
+
+    latest = 0
+    inspected_files = []
+
+    for root_dir, dirs, files in os.walk(directory):
+        if matches(root_dir):
+            dirs[:] = []
+            continue
+
+        for file in files:
+            file_path = os.path.join(root_dir, file)
+
+            if not matches(file_path):
+                latest = max(latest, os.path.getmtime(file_path))
+                inspected_files.append(file_path)
+
+    log.info(
+        "inspected files",
+        inspected_files=inspected_files,
+    )
+    return latest
+
+
+def is_js_build_up_to_date():
+    if not BUILD_STATE_FILE.exists():
+        return False
+    build_mtime = os.path.getmtime(BUILD_STATE_FILE)
+    web_latest_mtime = get_latest_mtime(root / "web")
+    return web_latest_mtime <= build_mtime
 
 
 def wait_for_javascript_build():
@@ -43,11 +86,20 @@ def wait_for_javascript_build():
         log.info("Waiting for JavaScript build to finish...")
         time.sleep(1)
 
+    log.info("JavaScript build finished successfully")
+
 
 def start_js_build():
     "for integration tests, initiate a javascript build"
 
     if not is_local_testing():
+        return
+
+    global js_build_success
+
+    if is_js_build_up_to_date():
+        log.info("JavaScript build is up to date. Skipping build.")
+        js_build_success = True
         return
 
     print(
@@ -66,7 +118,6 @@ $ just py_js-build
 """
     )
 
-    global js_build_success
     js_build_success = False
 
     log.info("Starting Javascript Build...")
@@ -90,6 +141,7 @@ $ just py_js-build
             js_build_success = False
         else:
             log.info("Javascript Build finished")
+            BUILD_STATE_FILE.touch()  # Update build state on success
             js_build_success = True
 
     # spin up a separate thread to monitor the build process
