828eac03736dbbc04645fb719b8bcf8ea393bdc1
Michael Bianco <mike@mikebian.co>
2025-08-03 21:52:52 +0000

    refactor: switch logging from loglevel to tslog for improved JSON support and config
    Generated-by: aiautocommit

--- web/app/configuration/logging.ts
+++ web/app/configuration/logging.ts
@@ -1,31 +1,52 @@
-import type { LogLevelNames } from "loglevel"
-import logger from "loglevel"
-import logLevelPrefix from "loglevel-plugin-prefix"
+/**
+ * Originally, we were using loglevel. tslog is typescript-by-default, has JSON support, and is a bit more modern.
+ */
+import { ILogObj, Logger } from "tslog"
 
-let isLoggingConfigured = false
+// ILogLevel
+import { isProduction } from "~/utils/environment"
 
-function configureLogging() {
-  if (isLoggingConfigured) return logger
-  isLoggingConfigured = true
+let loggerInstance: Logger<ILogObj> | undefined
 
-  // TODO extract out to separate logging config
-  // add expected log level prefixes
-  logLevelPrefix.reg(logger)
-  logger.enableAll()
-  logLevelPrefix.apply(logger)
-
-  if (import.meta.env.VITE_LOG_LEVEL) {
-    const logLevelFromEnv =
-      import.meta.env.VITE_LOG_LEVEL.toLowerCase() as LogLevelNames
-    logger.setLevel(logLevelFromEnv)
-  } else {
-    logger.setLevel(logger.levels.INFO)
-  }
-
-  return logger
+// TODO https://github.com/fullstack-build/tslog/pull/308
+// it's insane, but this mapping does not exist in tslog :/
+// I've removed the levels that are... silly
+const LOG_LEVEL_MAP: Record<string, number> = {
+  // silly: 0,
+  trace: 1,
+  debug: 2,
+  info: 3,
+  warn: 4,
+  error: 5,
+  fatal: 6,
+  // silent: 7,
 }
 
-configureLogging()
+function configureLogging() {
+  if (loggerInstance) return loggerInstance
 
-export const log = logger
-export default { log: logger }
+  const DEFAULT_LOG_LEVEL: number = isProduction()
+    ? LOG_LEVEL_MAP.warn
+    : LOG_LEVEL_MAP.info
+
+  loggerInstance = new Logger({
+    minLevel: DEFAULT_LOG_LEVEL,
+    type: "pretty",
+    prettyLogTemplate: "[{{hh}}:{{MM}}:{{ss}}] {{logLevelName}}: ",
+    hideLogPositionForProduction: isProduction(),
+  })
+
+  // determine the log level from the environment variable
+  if (import.meta.env.VITE_LOG_LEVEL) {
+    const logLevelFromEnv = import.meta.env.VITE_LOG_LEVEL.toLowerCase()
+    loggerInstance.settings.minLevel =
+      LOG_LEVEL_MAP[logLevelFromEnv] ?? DEFAULT_LOG_LEVEL
+  }
+
+  return loggerInstance
+}
+
+const log = configureLogging()
+
+export { log }
+export default { log }
