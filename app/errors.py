4e9aa145e54990243057c76b74cdc2a8b08ec60e
Michael Bianco <mike@mikebian.co>
2025-08-29 22:27:03 +0000

    feat: add ImpossibleStateError for unexpected conditions
    Generated-by: aiautocommit

--- app/errors.py
+++ app/errors.py
@@ -1,3 +1,14 @@
 """
 Custom errors for the application should go here
 """
+
+
+class ImpossibleStateError(Exception):
+    """
+    Raised when an impossible state is encountered. By definition, this should never happen.
+
+    It's useful to have as a distinct exception type to make (a) them easier to identify in Sentry and (b)
+    self-document the case in-code.
+    """
+
+    pass
