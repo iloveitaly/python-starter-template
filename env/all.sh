4a53e570b3d7e865f507c1c5e22ef09f4afd8d71
Michael Bianco <mike@mikebian.co>
2025-09-24 11:53:55 -0600

    docs: update environment variables in all.sh
    Added additional Python environment variables for configuration.

--- env/all.sh
+++ env/all.sh
@@ -2,11 +2,16 @@
 
 # standard python config
 export PYTHONFAULTHANDLER=1
+# never buffer output streams
 export PYTHONUNBUFFERED=1
+# ensures dict order is not relied upon
 export PYTHONHASHSEED=random
+# https://www.erdaltaskesen.com/blog/how-to-remove-pycache-folders-and-pyc-files/
+# https://github.com/pytest-dev/pytest/issues/200
 export PYTHONDONTWRITEBYTECODE=1
 # Don't automatically add PWD to PYTHONPATH
 export PYTHONSAFEPATH=1
 # Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
 export PYTHONIOENCODING='UTF-8'
+# without this, the timezone will change depending on the container/environment
 export TZ=UTC
