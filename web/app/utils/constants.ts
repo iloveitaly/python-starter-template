fcfeac2993642836a640acad0c2fd51eb9b02cb2
Michael Bianco <mike@mikebian.co>
2025-10-14 19:28:09 +0000

    feat: add NO_AUTOFILL_PROPS to prevent unwanted autofill
    Generated-by: aiautocommit

--- web/app/utils/constants.ts
+++ web/app/utils/constants.ts
@@ -24,3 +24,13 @@ export const IN_IFRAME =
         }
       })()
     : false // Default to false on server
+
+// 1p and others can improperly detect and autofill form fields, use `{...NO_AUTOFILL_PROPS}` to prevent this
+export const NO_AUTOFILL_PROPS = {
+  autoComplete: "off",
+  "data-1p-ignore": true,
+  "data-op-ignore": true,
+  "data-bwignore": true,
+  "data-lpignore": "true",
+  "data-form-type": "other",
+}
