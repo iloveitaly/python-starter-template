4792a932a96c566e792ca0cc8e5ac02162ef83b3
Michael Bianco <mike@mikebian.co>
2026-02-17 01:53:59 +0000

    feat: bootstrap.sh to automate mise agent setup
    Generated-by: aiautocommit

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+###############################
+# Bootstrap Agent Environments
+###############################
+
+# mise setup
+curl https://mise.run | sh
+mise trust
+mise install
+source <(mise activate)
