10f18bb982ed6d58b3cb15483bd066a3c6bca812
Michael Bianco <mike@mikebian.co>
2026-07-28 20:26:08 +0000

    chore(routes): update generated typed routes for api_app
    Generated-by: aiautocommit

--- app/generated/fastapi_typed_routes.py
+++ app/generated/fastapi_typed_routes.py
@@ -8,45 +8,11 @@ from app.server import api_app
 
 
 @overload
-def api_app_url_path_for(name: Literal["active_user_status"], **path_params) -> str: ...
-
-
-@overload
-def api_app_url_path_for(name: Literal["application_data"], **path_params) -> str: ...
-
-
-@overload
-def api_app_url_path_for(name: Literal["external_api_ping"], **path_params) -> str: ...
-
-
-@overload
 def api_app_url_path_for(name: Literal["frontend_handler"], **path_params) -> str: ...
 
 
 @overload
-def api_app_url_path_for(name: Literal["healthcheck"], **path_params) -> str: ...
-
-
-@overload
-def api_app_url_path_for(name: Literal["index"], **path_params) -> str: ...
-
-
-@overload
 def api_app_url_path_for(name: Literal["javascript_index"], **path_params) -> str: ...
-
-
-@overload
-def api_app_url_path_for(name: Literal["login_as_user"], **path_params) -> str: ...
-
-
-@overload
-def api_app_url_path_for(
-    name: Literal["unauthenticated_ping"], **path_params
-) -> str: ...
-
-
-@overload
-def api_app_url_path_for(name: Literal["user_list"], **path_params) -> str: ...
 
 
 def api_app_url_path_for(name: str, **path_params) -> str:
